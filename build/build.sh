b268dd4961c98f26bdec21cede763c7ce82c3916
Christian Mauderer <oss@c-mauderer.de>
2016-11-04 22:07:40 +0100

    build: Do bootstrap after building tools.

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,8 @@ SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 PROJECTDIR="${SCRIPTDIR}/../"
 
 source "${SCRIPTDIR}/configuration.sh"
+BUILD_DIR="${PROJECTDIR}/build/b-$BSP_NAME"
+export PATH="${PREFIX}/bin:${PATH}"
 
 # init git submodules
 cd "${PROJECTDIR}"
@@ -19,13 +21,12 @@ cd "${LIBBSD_SOURCE_DIR}"
 git submodule init
 git submodule update rtems_waf
 
-# bootstrap rtems
-cd "${RTEMS_SOURCE_DIR}"
-./bootstrap
-
 # build parts
 cd "${PROJECTDIR}"
 "${SCRIPTDIR}/build-toolchain.sh"
+cd "${RTEMS_SOURCE_DIR}"
+./bootstrap
+cd "${PROJECTDIR}"
 "${SCRIPTDIR}/build-bsp.sh" clean install
 "${SCRIPTDIR}/build-libbsd.sh"
 "${SCRIPTDIR}/build-libinih.sh"
