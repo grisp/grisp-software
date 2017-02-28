4c1a8fa009b55bcc1205c2fb3f3af00198594124
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-02-28 12:24:20 +0100

    Remove obsolete bsd command

--- grisp-sd-sample/init.c
+++ grisp-sd-sample/init.c
@@ -150,7 +150,5 @@ Init(rtems_task_argument arg)
 #include <rtems/netcmds-config.h>
 #define CONFIGURE_SHELL_COMMANDS_INIT
 #define CONFIGURE_SHELL_COMMANDS_ALL
-#define CONFIGURE_SHELL_USER_COMMANDS \
-  &rtems_shell_BSD_Command
 
 #include <rtems/shellconfig.h>
