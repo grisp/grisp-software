e1ee3cc07ae378ef08e98c6447d868a786943555
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2017-04-10 16:50:55 +0200

    libgrisp: Update pin-config for V1.1.

--- libgrisp/include/grisp/pin-config.h
+++ libgrisp/include/grisp/pin-config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 embedded brains GmbH.  All rights reserved.
+ * Copyright (c) 2016-2017 embedded brains GmbH.  All rights reserved.
  *
  *  embedded brains GmbH
  *  Dornierstr. 4
@@ -40,37 +40,44 @@ extern "C" {
 #endif /* __cplusplus */
 
 #define GRISP_LED_1R	{PIO_PA24, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
-#define GRISP_LED_1G	{PIO_PA22, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
+#define GRISP_LED_1G	{PIO_PA17, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
 #define GRISP_LED_1B	{PIO_PA23, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
-#define GRISP_LED_2R	{PIO_PA21, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
-#define GRISP_LED_2G	{PIO_PA19, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
-#define GRISP_LED_2B	{PIO_PA17, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
+#define GRISP_LED_2R	{PIO_PA13, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
+#define GRISP_LED_2G	{PIO_PA5 , PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
+#define GRISP_LED_2B	{PIO_PA11, PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}
 
 #define GRISP_PIN_CONFIG                                                      \
   /* Console */                                                               \
   {PIO_PD25C_URXD2 | PIO_PD26C_UTXD2,                                         \
       PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT},                              \
                                                                               \
-  /* SDRAM */                                                                 \
+  /* SDRAM, EBI */                                                            \
   {0x000000FF, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* D0 .. D7 */      \
   {0x0000003F, PIOE, ID_PIOE, PIO_PERIPH_A, PIO_DEFAULT}, /* D8 .. D13 */     \
   {0x00018000, PIOA, ID_PIOA, PIO_PERIPH_A, PIO_DEFAULT}, /* D14 .. D15 */    \
-  {0xBFF00000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* A0 .. A9, A11 */ \
-  {0x00002000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* A10 */           \
-  {0x00040000, PIOA, ID_PIOA, PIO_PERIPH_C, PIO_DEFAULT}, /* A12 */           \
-  {0x00100001, PIOA, ID_PIOA, PIO_PERIPH_C, PIO_DEFAULT}, /* BA0, BA1 */      \
-  {0x00008000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* CS */            \
-  {0x00040000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* LDQM / NBS0 */   \
-  {0x00008000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* UDQM / NBS1 */   \
-  {0x00004000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* CKE */           \
-  {0x00010000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAS */           \
-  {0x00020000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* CAS */           \
-  {0x00800000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* CLK */           \
-  {0x20000000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* WE */            \
+  {0xFFF80000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* (SAF_A1 .. SAF_A13) == (n.u., RAM_A0 .. RAM_A9, n.u., RAM_A11) */ \
+  {0x00002000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_A10 */       \
+  {0x00040000, PIOA, ID_PIOA, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_A12 == SAF_A14 */ \
+  {0x00180001, PIOA, ID_PIOA, PIO_PERIPH_C, PIO_DEFAULT}, /* (SAF_A15 .. SAF_A17) == (n.u., RAM_BA0, RAM_BA1) */ \
+  {0x00008000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* RAM_CS */        \
+  {0x00040000, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* RAM_DQML / NBS0 */ \
+  {0x00008000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_DQMH / NBS1 */ \
+  {0x00004000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_CKE */       \
+  {0x00010000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_RAS */       \
+  {0x00020000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_CAS */       \
+  {0x00800000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_CLK */       \
+  {0x20000000, PIOD, ID_PIOD, PIO_PERIPH_C, PIO_DEFAULT}, /* RAM_WE */        \
+  {PIO_PA1,    PIOA, ID_PIOA, PIO_INPUT,    PIO_PULLUP }, /* SAF_HC_DACK */   \
+  {PIO_PA12,   PIOA, ID_PIOA, PIO_OUTPUT_0, PIO_DEFAULT}, /* SAF_RESET_N */   \
+  {PIO_PC8A_NWE, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* SAF_WR */      \
+  {PIO_PC11A_NRD, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* SAF_RD */     \
+  {PIO_PC14A_NCS0, PIOC, ID_PIOC, PIO_PERIPH_A, PIO_DEFAULT}, /* SAF_CS */    \
+  {PIO_PC16,   PIOC, ID_PIOC, PIO_INPUT,    PIO_PULLUP }, /* SAF_IRQ */       \
+  {PIO_PD10,   PIOD, ID_PIOD, PIO_INPUT,    PIO_PULLUP }, /* SAF_DREQ */      \
                                                                               \
   /* Trace port */                                                            \
-  {0x000000F0, PIOC, ID_PIOC, PIO_PERIPH_C, PIO_DEFAULT}, /* TRACE0..3 */     \
-  {0x00000100, PIOC, ID_PIOC, PIO_PERIPH_D, PIO_DEFAULT}, /* TRACECLK */      \
+  {0x000000F0, PIOD, ID_PIOC, PIO_PERIPH_C, PIO_DEFAULT}, /* TRACE0..3 */     \
+  {0x00000100, PIOD, ID_PIOC, PIO_PERIPH_D, PIO_DEFAULT}, /* TRACECLK */      \
                                                                               \
   /* SD-Card */                                                               \
   {PIO_PA25D_MCCK, PIOA, ID_PIOA, PIO_PERIPH_D, PIO_DEFAULT},                 \
@@ -92,12 +99,15 @@ extern "C" {
   {PIO_PD20B_SPI0_MISO | PIO_PD21B_SPI0_MOSI | PIO_PD22B_SPI0_SPCK |          \
       PIO_PD27B_SPI0_NPCS3, PIOD, ID_PIOD, PIO_PERIPH_B, PIO_DEFAULT},        \
                                                                               \
-  /* USB Power Disable */                                                     \
-  {PIO_PC16, PIOC, ID_PIOC, PIO_OUTPUT_1, PIO_DEFAULT},                       \
+  /* WLAN Enable */                                                           \
+  {PIO_PA22, PIOA, ID_PIOA, PIO_OUTPUT_1, PIO_DEFAULT},                       \
                                                                               \
   /* Jumpers */                                                               \
-  {PIO_PC14 | PIO_PC11 | PIO_PC10 | PIO_PC9 | PIO_PC8,                        \
-      PIOC, ID_PIOC, PIO_INPUT, PIO_PULLUP},                                  \
+  {PIO_PA6 , PIOA, ID_PIOA, PIO_INPUT, PIO_PULLUP},                           \
+  {PIO_PD9 , PIOD, ID_PIOD, PIO_INPUT, PIO_PULLUP},                           \
+  {PIO_PC17, PIOC, ID_PIOC, PIO_INPUT, PIO_PULLUP},                           \
+  {PIO_PD11, PIOD, ID_PIOD, PIO_INPUT, PIO_PULLUP},                           \
+  {PIO_PC10, PIOC, ID_PIOC, PIO_INPUT, PIO_PULLUP},                           \
                                                                               \
   /* LEDs */                                                                  \
   GRISP_LED_1R, GRISP_LED_1G, GRISP_LED_1B,                                   \
