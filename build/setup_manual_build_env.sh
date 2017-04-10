9d5e671710762aa7dac3b351d70b61ea891d8921
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2017-04-10 14:44:52 +0200

    build: Add script for setting system dev environment.
    This script is not necessary for application development but
    can be usefull for RTEMS system development.

---
+++ build/setup_manual_build_env.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+
+# ****************************************
+# NORMALLY YOU SHOULD NOT NEED THIS SCRIPT
+# ****************************************
+
+# This script set's up an environment that can be used for building RTEMS for
+# GRiSP manually. This is only necessary for RTEMS system development. You
+# should never need it to build an application.
+
+# be more verbose
+set -x
+# exit on wrong command and undefined variables
+set -e -u
+
+# find out own directory
+SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+PROJECTDIR="${SCRIPTDIR}/../"
+
+# Configuration
+source "${SCRIPTDIR}/configuration.sh"
+BUILD_DIR="${PROJECTDIR}/build/b-$BSP_NAME"
+export PATH="${PREFIX}/bin:${PATH}"
+
+export PREPROMPT="%B(GRISP-RTEMS)%b "
+
+${SHELL}
