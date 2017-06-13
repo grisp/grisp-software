71cf25c53c4b29e82fcb0f96a8d759ea8ea43c1c
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2017-06-13 10:15:56 +0200

    openocd: Use release 0.10.0.
    Using the tar.bz2 instead of the git repo has the advantage that no
    special autotool version is necessary.

--- build/build-openocd.sh
+++ build/build-openocd.sh
@@ -11,12 +11,49 @@ PROJECTDIR="${SCRIPTDIR}/../"
 
 # configuration
 source "${SCRIPTDIR}/configuration.sh"
-# Do not use rtems autoconfig. It is lacking libtool and pkg-config.
-# export PATH="${PREFIX}/bin:${PATH}"
 
 # Build openocd
-cd "${PROJECTDIR}/openocd"
-git clean -dxf .
-./bootstrap
-./configure --prefix="${PREFIX}"
-make install
+cd ${SCRIPTDIR}
+rm -rf ./b-openocd
+mkdir -p b-openocd
+cd ${SCRIPTDIR}/b-openocd
+tar -xaf ${SCRIPTDIR}/src/openocd-0.10.0.tar.bz2
+cd "openocd-0.10.0"
+# Disable most options. It will only increase the build time and the binary.
+./configure --prefix="${PREFIX}" \
+	--enable-ftdi \
+	--disable-stlink \
+	--disable-ti-icdi \
+	--disable-ulink \
+	--disable-usb-blaster-2 \
+	--disable-vsllink \
+	--disable-osbdm \
+	--disable-opendous \
+	--disable-aice \
+	--disable-usbprog \
+	--disable-rlink \
+	--disable-armjtagew \
+	--disable-cmsis-dap \
+	--disable-usb-blaster \
+	--disable-presto \
+	--disable-openjtag \
+	--disable-jlink \
+	--disable-parport \
+	--disable-parport-ppdev \
+	--disable-parport-giveio \
+	--disable-jtag_vpi \
+	--disable-amtjtagaccel \
+	--disable-zy1000-master \
+	--disable-zy1000 \
+	--disable-ioutil \
+	--disable-ep93xx \
+	--disable-at91rm9200 \
+	--disable-bcm2835gpio \
+	--disable-gw16012 \
+	--disable-oocd_trace \
+	--disable-buspirate \
+	--disable-sysfsgpio \
+	--disable-minidriver-dummy \
+	--disable-remote-bitbang
+
+make -j `nproc` install
