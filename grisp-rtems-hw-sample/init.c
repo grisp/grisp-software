25a31856053d2ad2a2fa3823cbe9d2e3f6e1030d
Christian Mauderer <Christian.Mauderer@embedded-brains.de>
2017-08-29 09:21:51 +0200

    grisp-rtems-hw-sample: Reorganize. Add UART.

--- grisp-rtems-hw-sample/init.c
+++ grisp-rtems-hw-sample/init.c
@@ -35,6 +35,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 #include <sys/ioctl.h>
 
@@ -50,6 +51,8 @@
 
 #include <grisp/led.h>
 #include <grisp/pin-config.h>
+
+#define N_ELE(arr) (sizeof(arr)/sizeof(arr[0]))
 
 /** Page size in bytes */
 #define EEPROM_PAGE_SIZE 16
@@ -84,11 +87,6 @@
 static const Pin DPIO1[] = {DIO11, DIO21, DIO31, DIO41, DIO51, DIO61, DIO71, DIO81};
 static const Pin DPIO2[] = {DIO12, DIO22, DIO32, DIO42, DIO52, DIO62, DIO72, DIO82};
 
-uint32_t ans;
-
-
-
-
 const Pin atsam_pin_config[] = {GRISP_PIN_CONFIG};
 const size_t atsam_pin_config_count = PIO_LISTSIZE(atsam_pin_config);
 const uint32_t atsam_matrix_ccfg_sysio = GRISP_MATRIX_CCFG_SYSIO;
@@ -97,6 +95,7 @@ static uint8_t test_rx_array[3];
 static uint8_t test_tx_array[3];
 static const char eeprom_path[] = "/dev/i2c-0.eeprom-0";
 static const char cmps_path[] = "/dev/i2c-0.compass-0";
+static const char uart_path[] = "/dev/ttyUSART0";
 
 static int
 init_spi(void)
@@ -152,31 +151,6 @@ read_spi_register(int fd, uint8_t addr, uint8_t *val)
 }
 
 static void
-init_i2c(int *fd_in, int *fd_out)
-{
-	int rv;
-
-	rv = atsam_register_i2c_0();
-	assert(rv == 0);
-
-	rv = i2c_dev_register_eeprom(
-	    ATSAM_I2C_0_BUS_PATH,
-	    &eeprom_path[0],
-	    EEPROM_ADDR,
-	    1,
-	    EEPROM_PAGE_SIZE,
-	    EEPROM_SIZE,
-	    0
-	);
-	assert(rv == 0);
-
-	*fd_in = open(&eeprom_path[0], O_RDWR);
-	assert(*fd_in != -1);
-	*fd_out = open(&eeprom_path[0], O_RDWR);
-	assert(*fd_out != -1);
-}
-
-static void
 init_cmps(int *fd_in, int *fd_out)
 {
 	int rv;
@@ -196,40 +170,6 @@ init_cmps(int *fd_in, int *fd_out)
 	assert(*fd_in != -1);
 	*fd_out = open(&cmps_path[0], O_RDWR);
 	assert(*fd_out != -1);
-}
-
-static int
-unlink_test_eeprom_and_bus(void)
-{
-	int rv;
-
-	rv = unlink(ATSAM_I2C_0_BUS_PATH);
-	assert(rv == 0);
-
-	rv = unlink(&eeprom_path[0]);
-	assert(rv == 0);
-
-	rv= unlink(&cmps_path[0]);
-	assert(rv == 0);
-
-	return rv;
-}
-
-static int
-close_fds(int in, int out, int in_cmps, int out_cmps)
-{
-	int rv;
-
-	rv = close(in);
-	assert(rv == 0);
-	rv = close(out);
-	assert(rv == 0);
-	rv = close(in_cmps);
-	assert(rv == 0);
-	rv = close(out_cmps);
-	assert(rv == 0);
-
-	return rv;
 }
 
 static void
@@ -266,105 +206,179 @@ init_gpio2(void) {
 }
 
 static void
-Init(rtems_task_argument arg)
+prepare_i2c(void)
 {
 	int rv;
+	rv = atsam_register_i2c_0();
+	assert(rv == 0);
+}
+
+static void
+destroy_i2c(void)
+{
+	int rv;
+	rv = unlink(ATSAM_I2C_0_BUS_PATH);
+	assert(rv == 0);
+}
+
+static bool
+test_spi(void)
+{
+	int err = 0;
 	int fd_spi;
-	int fd_in, fd_out;
-	int fd_in_cmps, fd_out_cmps;
+	const uint8_t addr[] = {0x0, 0x1, 0x2, 0x3, 0x8, 0x9, 0xA, 0xB};
+	const uint8_t acl2_compare_value[] = {0xad, 0x1d, 0xf2};
 	uint8_t val;
-	uint8_t addr[] = {0x0, 0x1, 0x2, 0x3, 0x8, 0x9, 0xA, 0xB};
-	uint8_t acl2_compare_value[] = {0xad, 0x1d, 0xf2};
-	uint8_t cmps_compare_value[] = {0x48, 0x34, 0x33};
-	uint8_t in[] = {0xc0, 0x1d, 0xc0, 0xff, 0xee};
-	uint8_t out[] = {0xff, 0xff, 0xff, 0xff, 0xff};
-	uint8_t cmps_in[] = {0x09};
-	uint8_t cmps_out[] = {0xff,0xff, 0xff, 0xff};
-	int i, err = 0;
-	bool passed = true;
+	size_t i;
 
-	(void)arg;
-	puts("");
-	puts("Start testing Grisp hardware");
-	puts("Contect the following modules: PmodACL2 on SPI1");
-	puts("                               PmodCMPS on I2C");
-
-	puts("\nSPI test");
+	puts("----- SPI test");
 	fd_spi = init_spi();
-	for(i=0; i<8; i++) {
+	for (i=0; i < N_ELE(addr); i++) {
 		read_spi_register(fd_spi, addr[i], &val);
 		printf("value of register %x: %x\n",addr[i], val);
-		if(i < 3) {
+		if(i < N_ELE(acl2_compare_value)) {
 			if (val != acl2_compare_value[i]) {
 				err = 1;
 			}
 		}
 	}
 	if (err != 0) {
-		puts("***** SPI test FAILED. Try right module PmodACL2 on port SPI1.");
-		passed = false;
+		puts("EEEEE SPI test FAILED. Try right module PmodACL2 on port SPI1.");
 	}
 	else {
-		puts("SPI test passed.");
+		puts("***** SPI test passed.");
+		return true;
 	}
 
-	err = 0;
-	puts("\nI2C test");
-	init_i2c(&fd_in, &fd_out);
+	return false;
+}
 
-	read_write_i2c_eeprom(fd_in, fd_out, &in[0], &out[0], sizeof (in)/sizeof (in[0]));
+static bool
+test_i2c_eeprom(void)
+{
+	int err = 0;
+	int fd_in, fd_out;
+	uint8_t in[] = {0xc0, 0x1d, 0xc0, 0xff, 0xee};
+	uint8_t out[] = {0xff, 0xff, 0xff, 0xff, 0xff};
+	int rv;
+	size_t i;
+
+	puts("----- Internal I2C test");
+
+	rv = i2c_dev_register_eeprom(
+	    ATSAM_I2C_0_BUS_PATH,
+	    &eeprom_path[0],
+	    EEPROM_ADDR,
+	    1,
+	    EEPROM_PAGE_SIZE,
+	    EEPROM_SIZE,
+	    0
+	);
+	assert(rv == 0);
+
+	fd_in = open(&eeprom_path[0], O_RDWR);
+	assert(fd_in != -1);
+	fd_out = open(&eeprom_path[0], O_RDWR);
+	assert(fd_out != -1);
+
+	read_write_i2c_eeprom(fd_in, fd_out, &in[0], &out[0], N_ELE(in));
 
 	printf("I2C eeprom write data: ");
-	for (i=0; i<(int)(sizeof (in)/sizeof (in[0])); i++) {
+	for (i=0; i < N_ELE(in); i++) {
 		printf("%x ",in[i]);
 	}
 	puts("");
 	printf("I2C eeprom read data:  ");
-	for (i=0; i<(int)(sizeof (in)/sizeof (in[0])); i++) {
+	for (i=0; i < N_ELE(in); i++) {
 		printf("%x ",out[i]);
 	}
-	for (i=0; i<(int)(sizeof (in)/sizeof (in[0])); i++) {
+	puts("");
+	for (i=0; i < N_ELE(in); i++) {
 		if (in[i] != out[i]) {
 			err = 1;
 		}
 	}
 	puts("");
+
+	rv = close(fd_in);
+	assert(rv == 0);
+	rv = close(fd_out);
+	assert(rv == 0);
+	rv = unlink(&eeprom_path[0]);
+	assert(rv == 0);
+
 	if (err != 0) {
-		puts("***** Internal I2C test FAILED.");
-		passed = false;
+		puts("EEEEE Internal I2C test FAILED.");
 	}
 	else {
-		puts("Internal I2C test passed.");
+		puts("***** Internal I2C test passed.");
+		return true;
 	}
-	puts("");
+
+	return false;
+}
+
+static bool
+test_i2c_cmps(void)
+{
+	int err = 0;
+	int fd_in_cmps, fd_out_cmps;
+	uint8_t cmps_compare_value[] = {0x48, 0x34, 0x33};
+	uint8_t cmps_in[] = {0x09};
+	uint8_t cmps_out[] = {0xff,0xff, 0xff, 0xff};
+	size_t i;
+	int rv;
+
+	puts("----- External I2C test");
 
 	init_cmps(&fd_in_cmps, &fd_out_cmps);
 
 	read_write_pmod_cmps(fd_in_cmps, &cmps_out[0], sizeof (cmps_out)/sizeof(cmps_out[0]));
 
-	printf("\nCompass register read start: ");
-	for (i=0; i<(int)(sizeof (cmps_in)/sizeof (cmps_in[0])); i++) {
+	printf("Compass register read start: ");
+	for (i=0; i < N_ELE(cmps_in); i++) {
 		printf("%x ",cmps_in[i]);
 	}
 	puts("");
-	printf("Compass register values : ");
-	for (i=0; i<(int)(sizeof (cmps_out)/sizeof (cmps_out[0])); i++) {
+
+	printf("Compass register values: ");
+	for (i=0; i < N_ELE(cmps_out); i++) {
 		printf("%x ",cmps_out[i]);
 	}
-	for (i=0; i<3; i++) {
+	puts("");
+	for (i=0; i < 3; i++) {
 		if (cmps_out[i+1] != cmps_compare_value[i]) {
 			err = 1;
 		}
 	}
-	puts("");
+
+	rv = close(fd_in_cmps);
+	assert(rv == 0);
+
+	rv = close(fd_out_cmps);
+	assert(rv == 0);
+
+	rv = unlink(&cmps_path[0]);
+	assert(rv == 0);
+
 	if (err != 0) {
-		puts("***** External I2C test FAILED. Try right module PmodCMPS on I2C.");
-		passed = false;
+		puts("EEEEE External I2C test FAILED. Try right module PmodCMPS on I2C.");
 	}
 	else {
-		puts("External I2C test passed.");
+		puts("***** External I2C test passed.");
+		return true;
 	}
-	puts("");
+
+	return false;
+}
+
+static bool
+test_gpio(void)
+{
+	bool passed = true;
+	uint32_t ans;
+
+	puts("----- GPIO test");
 
 	init_gpio1();
 
@@ -492,6 +506,105 @@ Init(rtems_task_argument arg)
 		}
 	}
 
+	if (!passed) {
+		puts("EEEEE GPIO test FAILED.");
+	}
+	else {
+		puts("***** GPIO test passed.");
+	}
+
+	return passed;
+}
+
+static bool
+test_uart(void)
+{
+	bool passed = true;
+	int fd;
+	int rv;
+	struct termios options;
+	char rx = '\0';
+
+	puts("----- UART test");
+
+	fd = open(uart_path, O_RDWR);
+	assert(fd != -1);
+
+	/* Setup interface */
+	rv = tcgetattr(fd, &options);
+	assert(rv == 0);
+	rv = cfsetispeed(&options, B19200);
+	assert(rv == 0);
+	rv = cfsetospeed(&options, B19200);
+	assert(rv == 0);
+	options.c_cflag |= CRTSCTS;
+	rv = tcsetattr(fd, TCSANOW, &options);
+	assert(rv == 0);
+
+	rv = write(fd, "x", 1);
+	if (rv != 1) {
+		passed = false;
+		perror("uart write failed");
+	}
+
+	if (passed) {
+		rv = read(fd, &rx, 1);
+		if (rv != 1) {
+			perror("uart read failed");
+			passed = false;
+		} else if (rx != 'x') {
+			passed = false;
+			printf("received wrong character: %c\n", rx);
+		}
+	}
+
+	rv = close(fd);
+	assert(rv == 0);
+
+	if (!passed) {
+		puts("EEEEE UART test FAILED.");
+	}
+	else {
+		puts("***** UART test passed.");
+	}
+
+	return passed;
+}
+
+static void
+Init(rtems_task_argument arg)
+{
+	bool passed = true;
+
+	(void)arg;
+	puts("");
+	puts("Start testing Grisp hardware");
+	puts("Contect the following modules:");
+	puts(" - PmodACL2 on SPI1");
+	puts(" - PmodCMPS on I2C");
+	puts(" - Bridge between GPIO1 and GPIO2");
+	puts(" - Loopback on UART (TxD -> RxD, RTS -> CTS)");
+
+	grisp_led_set1(false, false, false);
+	grisp_led_set2(false, false, false);
+
+	if (passed) {
+		passed = test_spi();
+	}
+	prepare_i2c();
+	if (passed) {
+		passed = test_i2c_eeprom();
+	}
+	if (passed) {
+		passed = test_i2c_cmps();
+	}
+	destroy_i2c();
+	if (passed) {
+		passed = test_gpio();
+	}
+	if (passed) {
+		passed = test_uart();
+	}
 	if (passed) {
 		puts("\nLED test");
 		grisp_led_set1(true, true, true);
@@ -505,12 +618,11 @@ Init(rtems_task_argument arg)
 		puts("**** Test FAILED ****");
 	}
 
-	rv = unlink_test_eeprom_and_bus();
-	assert(rv == 0);
-	rv = close_fds(fd_in, fd_out, fd_in_cmps, fd_out_cmps);
-	assert(rv == 0);
-	rtems_task_delete(RTEMS_SELF);
+	while(true) {
+		sleep(1);
+	}
 
+	rtems_task_delete(RTEMS_SELF);
 }
 
 /*
