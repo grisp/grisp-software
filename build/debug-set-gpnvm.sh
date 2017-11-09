73d29967bdd8000e896a3fd721b9e1888940ac89
Andreas Leibold <andreas.leibold@embedded-brains.de>
2017-11-09 11:27:53 +0100

    debug: Move setting gpnvm to own script.

--- build/debug-set-gpnvm.sh
+++ build/debug-set-gpnvm.sh
@@ -9,9 +9,6 @@ set -e -u
 SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 PROJECTDIR="${SCRIPTDIR}/../"
 
-ELFFILE=$1
-
 ${SCRIPTDIR}/debug-start-openocd.sh \
-	-c "reset" -c "halt" -c "program \"${ELFFILE}\"" \
-	-c "reset" -c "halt" -c "verify_image_checksum \"${ELFFILE}\"" \
+	-c "reset" -c "halt" -c "mww 0x400e0c04 0x5a00010b"\
 	-c "reset" -c "exit"
